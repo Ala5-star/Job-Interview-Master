92f1cee0bb5a0a073fd0efa5db0e714a6fbc78a9
Ala5-star <aldona.magda@gmail.com>
2024-12-01 20:50:07 +0100

    Initial Commit

---
+++ Job Interview MasterTests/Job_Interview_MasterTests.swift
@@ -0,0 +1,17 @@
+//
+//  Job_Interview_MasterTests.swift
+//  Job Interview MasterTests
+//
+//  Created by AB on 01/12/2024.
+//
+
+import Testing
+@testable import Job_Interview_Master
+
+struct Job_Interview_MasterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
